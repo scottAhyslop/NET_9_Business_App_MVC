0935de925406c433c6469e7bf732b67e5d6a2c66
Scott A. Hyslop <sahyslop@gmail.com>
2025-04-11 17:52:43 -0400

    Added file handling types to DepartmentsController
    Added three different endpoints to the DepartmentsController to handle virtual files (within the root directory), physical files (on PC), or Content files (content from anywhere)

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NET_9_Business_App_MVC.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NET_9_Business_App_MVC.Controllers
 {
@@ -18,7 +19,7 @@ namespace NET_9_Business_App_MVC.Controllers
         [HttpGet]
         [Route("/departments")]
         public IActionResult Index()//home View for the DepartmentsController
-        {         
+        {
             //display sample data, make it look "pretty"
             string output = "<style>.results{position: absolute;top: 25%;left: 50%;margin-right: -50%;transform: translate(-50%, -33%)}.center{margin-left:33%;}</style><h2 class='center'>Department Info Page</h2>";
 
@@ -30,7 +31,7 @@ namespace NET_9_Business_App_MVC.Controllers
             }
             output += "</h4><br/>";
             return Content(output, "text/html");
-        }
+        } //working IAction
 
         //Details page for the DepartmentsController
         [HttpGet]
@@ -48,7 +49,7 @@ namespace NET_9_Business_App_MVC.Controllers
                 return department;
             }
             return "Department was nuller than null";
-           
+
         }//End Details   //working
 
         //Create page for the DepartmentsController
@@ -66,7 +67,7 @@ namespace NET_9_Business_App_MVC.Controllers
             }
 
             ModelState.AddModelError("Description", "Description is required...");
-            
+
             if (department is null)
             {
                 return "Department is null";
@@ -84,9 +85,9 @@ namespace NET_9_Business_App_MVC.Controllers
         }//End Create  //Working
 
         [HttpPost]
-        public object Edit([FromBody]Department department)
+        public object Edit([FromBody] Department department)
         {
-            
+
             var dep = departments.FirstOrDefault(dept => dept.DepartmentId == department.DepartmentId);
             if (department is null)
             {
@@ -124,6 +125,29 @@ namespace NET_9_Business_App_MVC.Controllers
             return "Nuller than null";*/
 
         }//end delete
+
+        //File download - virtual
+        [Route("/download_vf")]//virtual file downloader (@webroot folder)
+        public IActionResult ReturnVirtualFile() 
+        {
+            return File("/somethingSomething.txt", "text/plain");
+        }
+        //File download - physical
+        [Route("/download_pf")]//physical file downloader (outside of @webroot folder)
+        public IActionResult ReturnPhysicalFile()
+        {
+            return PhysicalFile(@"c:\Users\dredg\Downloads\somethingSomething.txt", "text/plain");
+        }//"C:\Users\dredg\Downloads\somethingSomething.txt"
+
+        //File download - Content
+        [Route("/download_cf")]//Content file downloader, getting Content from server..,
+        public IActionResult ReturnContentFile()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes("c:\\Users\\dredg\\Downloads\\somethingSomething.txt");
+
+            return File(bytes, "text/plain");
+           
+        }//
     }//end DepartmentsController         
 }//end namespace
 
